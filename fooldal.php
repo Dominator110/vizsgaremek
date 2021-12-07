726e95890c98a9c75c4e6df0525a3fe3f4d09bc1
Roland <rolandbardfalvi@gmail.com>
2021-12-07 08:27:30 +0100

    nyitó oldal

---
+++ fooldal.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="fooldal.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Főoldal</title>
+    
+</head>
+<body>
+<div id="fonavbar">
+    <ul>
+        <li><a href="#bejelentkezes">Bejelentkezés</a></li>
+        <li><a class="active" href="">Home</a></li>
+        <li id="hirek" ><a href="#">Hírek</a></li>
+        <li id="sport"><a href="#">Sport</a></li>
+        <li id="politika"><a href="#">Politika</a></li>
+        <li id="egeszseg"><a href="#">Egészség</a></li>
+        <li id="kulturalis"><a href="#">Kulturális</a></li>
+        <li id="idojaras"><a href="#">Időjárás</a></li>
+    </ul>
+</div>
+<div id="sportnavbar">
+    <ul id="sportul">
+        <li id="sportli"><a id="foci" href="">Foci</a></li>
+        <li id="sportli"><a id="kosar" href="">Kosárlabda</a></li>
+        <li id="sportli"><a id="ropi" href="">Röplabda</a></li>
+        <li id="sportli"><a id="kezi" href="">Kézilabda</a></li>
+        <li id="sportli"><a id="vizilabda" href="">Vizilabda</a></li>
+        <li id="sportli"><a id="f1" href="">Formula-1</a></li>
+    </ul>
+</div>
+
+
+
+
+
+<script src="fooldal.js"></script>
+</body>
+</html>
