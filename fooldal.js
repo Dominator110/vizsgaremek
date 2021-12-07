0266dd810a35a9a46b5dfe9f483d76563330c980
Dominator110 <92374606+Dominator110@users.noreply.github.com>
2021-12-07 14:55:04 +0100

    Add files via upload

--- fooldal.js
+++ fooldal.js
@@ -1,5 +1,54 @@
+// Sport menü elrejtése
 $("#sportnavbar").hide();
+//sport menu megjelenítése és a töbi elrejtése
 $("#sport").click(function(){
+    $("#egeszsegmenu").hide();
+    $("#Politikamenu").hide();
+    $("#Hirekmenu").hide();
+    $("#kulturalismenu").hide();
     $("#sportnavbar").toggle();
 })
+// Hirek menü elrejtése
+ $("#Hirekmenu").hide();
+ //sport menu megjelenítése és a töbi elrejtése
+ $("#hirek").click(function(){
+    $("#egeszsegmenu").hide();
+    $("#Politikamenu").hide();
+    $("#sportnavbar").hide();
+    $("#kulturalismenu").hide();
+     $("#Hirekmenu").toggle();  
+ })
+// Politika menü elrejtése
+ $("#Politikamenu").hide();
+ //Politika menu megjelenítése és a töbi elrejtése
+ $("#politika").click(function(){
+    $("#egeszsegmenu").hide();
+    $("#Hirekmenu").hide();
+    $("#sportnavbar").hide();
+    $("#kulturalismenu").hide();
+     $("#Politikamenu").toggle();  
+ })
+ // Egészség menü elrejtése
+ $("#egeszsegmenu").hide();
+ //Egészség menu megjelenítése és a töbi elrejtése
+ $("#egeszseg").click(function(){
+    $("#Hirekmenu").hide();
+    $("#Politikamenu").hide();
+    $("#sportnavbar").hide();
+    $("#kulturalismenu").hide();
+     $("#egeszsegmenu").toggle();  
+ })
+ // Kultura menü elrejtése
+ $("#kulturalismenu").hide();
+ //Kultura menu megjelenítése és a töbi elrejtése
+ $("#kulturalis").click(function(){
+    $("#egeszsegmenu").hide();
+    $("#Hirekmenu").hide();
+    $("#Politikamenu").hide();
+    $("#sportnavbar").hide();
+     $("#kulturalismenu").toggle();  
+ })
+
+
+
 
--- fooldal.php
+++ fooldal.php
@@ -7,12 +7,17 @@
     <link rel="stylesheet" href="fooldal.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Főoldal</title>
     
 </head>
 <body>
+<!-- főmenű -->
 <div id="fonavbar">
     <ul>
+        <div>
+            <img id="logo" src="logo.png" alt="Logo">
+        </div>
         <li><a href="#bejelentkezes">Bejelentkezés</a></li>
         <li><a class="active" href="">Home</a></li>
         <li id="hirek" ><a href="#">Hírek</a></li>
@@ -20,12 +25,22 @@
         <li id="politika"><a href="#">Politika</a></li>
         <li id="egeszseg"><a href="#">Egészség</a></li>
         <li id="kulturalis"><a href="#">Kulturális</a></li>
-        <li id="idojaras"><a href="#">Időjárás</a></li>
+        <li id="idojaras"><a href="idojaras.php" target="_blank">Időjárás</a></li>
+        <div class="topnav">
+            <div class="search-container">
+
+            <button type="submit"><i class="fa fa-search"></i></button>
+            <input type="text" placeholder="keresés..">
+            </div>
+
+            
+        </div>
     </ul>
 </div>
+<!-- sport menü -->
 <div id="sportnavbar">
     <ul id="sportul">
-        <li id="sportli"><a id="foci" href="">Foci</a></li>
+        <li id="sportli"><a id="Foci" href="">Foci</a></li>
         <li id="sportli"><a id="kosar" href="">Kosárlabda</a></li>
         <li id="sportli"><a id="ropi" href="">Röplabda</a></li>
         <li id="sportli"><a id="kezi" href="">Kézilabda</a></li>
@@ -33,6 +48,44 @@
         <li id="sportli"><a id="f1" href="">Formula-1</a></li>
     </ul>
 </div>
+<!-- Hírek menü -->
+<div id="Hirekmenu">
+    <ul id="hirekul">
+        <li id="hirekli"><a id="Home" href="">Home</a></li>
+        <li id="hirekli"><a id="Covid" href="">Covid</a></li>
+        <li id="hirekli"><a id="Tech" href="">Technologia</a></li>
+        <li id="hirekli"><a id="klima" href="">Klíma</a></li>
+        <li id="hirekli"><a id="tudomany" href="">Tudomány</a></li>
+        <li id="hirekli"><a id="Mo" href="">Magyarország</a></li>
+    </ul>
+</div>
+<!-- politika menu -->
+<div id="Politikamenu">
+    <ul id="politikaul">
+        <li id="politikali"><a id="Belfoldi" href="">Belföldi Politika</a></li>
+        <li id="politikali"><a id="Vilag" href="">Világ politika</a></li>
+    </ul>
+</div>
+<!-- egeszseg menu -->
+<div id="egeszsegmenu">
+    <ul id="egeszsegul">
+        <li id="egeszsegli"><a id="tap" href="">Táplálkozás</a></li>
+        <li id="egeszsegli"><a id="olt" href="">Öltözködés</a></li>
+        <li id="egeszsegli"><a id="mozgas" href="">Mozgás</a></li>
+    </ul>
+</div>
+<!-- kulturalis menu -->
+<div id="kulturalismenu">
+    <ul id="kulturalisul">
+        <li id="kulturalisli"><a id="tap" href="">Filmek</a></li>
+        <li id="kulturalisli"><a id="olt" href="">Művészet</a></li>
+        <li id="kulturalisli"><a id="mozgas" href="">Zene</a></li>
+        <li id="kulturalisli"><a id="mozgas" href="">Könyvek</a></li>
+        <li id="kulturalisli"><a id="mozgas" href="">TV</a></li>
+    </ul>
+</div>
+
+
 
 
 
