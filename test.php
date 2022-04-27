940a081e52c398ecff0b64dab9f54c6b9dc4a4c1
TheDunkinDodo <92374608+TheDunkinDodo@users.noreply.github.com>
2022-04-27 13:00:58 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+    require "login.php";
+    require "kapcs.php";
+    use PHPUnit\Framework\TestCase;
+
+    class test extends TestCase{
+
+        private $result;
+
+        public function testLogin():void{
+            $GLOBALS['username'] = "asd";
+            
+            $GLOBALS['secure_pass'] = "7815696ecbf1c96e6894b779456d330e";
+
+            Login();
+            $this->assertEquals(true, $_SESSION['login']);
+        }
+
+        public function testFalseLogin():void{
+            $GLOBALS['username'] = "asd";
+            
+            $GLOBALS['secure_pass'] = "asd";
+
+            Login();
+            $this->assertEquals(false, $_SESSION['login']);
+        }
+    }
+?>
